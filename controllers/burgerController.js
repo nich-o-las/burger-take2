83fc612dd98c70d37c86af089f242e453413428f
Nicholas Cox <nick.a.cox@gmail.com>
2019-12-18 11:42:10 -0600

    bug fixes

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -1,13 +1,11 @@
 var express = require("express");
 
 var router = express.Router();
-
-// Import the model (cat.js) to use its database functions.
-var burger = require("../models/burger.js");
+var db = require("../models");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-  burger.findAll({}).then(result => {
+  db.sandwich.findAll({}).then(result => {
     const hbObj = {
       burgers: result
     };
@@ -16,21 +14,21 @@ router.get("/", function(req, res) {
 });
 
 router.post("/", function(req, res) {
-  burger.create({
+  db.sandwich.create({
     description: req.body.description,
     validate: { len: [1] }
   }).then(() => res.redirect("/"));
 });
 
 router.put("/:id", function(req, res){
-  burger.update(
+  db.sandwich.update(
     {eaten: req.body.eaten},
     {where: {id: req.params.id}}
   ).then((result) => res.send(result))
 });
 
 router.delete("/:id", function(req, res){
-  burger.destroy(
+  db.sandwich.destroy(
     {where: {id: req.params.id}}
   ).then(result => res.sendStatus(500))
 });  
--- models/burger.js
+++ models/burger.js
@@ -1,25 +1,15 @@
-var Sequelize = require("sequelize");
-
-if (process.env.JAWSDB_URL){
-  var sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  var sequelize = new Sequelize("burger_db", "root", "password", {
-    dialect: "mysql",
-    host: "localhost"
-  });
-}
-
-
-const Burger = sequelize.define("sandwich", {
-  description: {
-    type: Sequelize.STRING,
-    validate:{
-      len: [01]
+module.exports = function(sequelize, DataTypes) {
+  const Burger = sequelize.define("sandwich", {
+    description: {
+      type: DataTypes.STRING,
+      validate: {
+        len: [01]
+      }
+    },
+    eaten: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
     }
-  },
-  eaten: { type: Sequelize.BOOLEAN, defaultValue: false }
-});
-
-Burger.sync();
-
-module.exports = Burger;
+  });
+  return Burger;
+};
